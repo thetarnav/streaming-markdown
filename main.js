e25cf4d5a2f4b4f04feeba64a4f79646c591559e
Damian Tarnawski <gthetarnav@gmail.com>
2024-02-19 15:05:43 +0100

    Cleanup

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 import * as md_stream from "./md_stream.js"
 
 async function main() {
-    const source_md_res = await fetch(location.href + "/readme.md")
-    const source_md = await source_md_res.text()
+    const source_res = await fetch(location.href + "/readme.md")
+    const source_txt = await source_res.text()
 
     const container = /** @type {HTMLElement} */(document.getElementById("markdown"))
     const stream = md_stream.stream(container)
 
     let i = 0
-    while (i < source_md.length) {
+    while (i < source_txt.length) {
         const length = Math.floor(Math.random() * 20) + 1
         const delay  = Math.floor(Math.random() * 80) + 10
-        const chunk  = source_md.slice(i, i += length)
+        const chunk  = source_txt.slice(i, i += length)
         md_stream.write(stream, chunk)
         await new Promise(resolve => setTimeout(resolve, delay))
     }
 
     md_stream.end(stream)
 }
-
 
 main()
 export {}
