6a69919812d53cbb9518d5285c0655f09f537a68
Damian Tarnawski <gthetarnav@gmail.com>
2024-03-12 19:36:14 +0100

    Emit a minified output for CDNs

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
-import fs   from "node:fs"
-import path from "node:path"
-import url  from "node:url"
-import ts   from "typescript"
+import fs       from "node:fs"
+import path     from "node:path"
+import url      from "node:url"
+import ts       from "typescript"
+import * as esb from "esbuild"
 
 const dirname       = path.dirname(url.fileURLToPath(import.meta.url))
 const file_js_path  = path.join(dirname, "smd.js")
@@ -24,14 +25,29 @@ const ts_options = {
 function main() {
 	const begin = performance.now()
 
+	/* Build minified version for the CDNs */
+	esb.build({
+		entryPoints: ["smd_min_entry.js"],
+		format: "esm",
+		platform: "browser",
+		bundle: true,
+		minify: true,
+		drop: ["console", "debugger"],
+		outfile: "smd.min.js",
+	}).then(() => {
+		console.log(`MIN complete at ${Math.ceil(performance.now() - begin)}ms`)
+	})
+	
+	
+	/* DTS */
 	// Remove old .d.ts files
 	if (fs.existsSync(file_dts_path)) fs.unlinkSync(file_dts_path)
 	if (fs.existsSync(file_map_path)) fs.unlinkSync(file_map_path)
 	
 	// Emit d.ts files
-    const program = ts.createProgram([file_js_path], ts_options)
-    program.emit()
-    console.log(`DTS complete in ${Math.ceil(performance.now() - begin)}ms`)
+	const program = ts.createProgram([file_js_path], ts_options)
+	program.emit()
+	console.log(`DTS complete at ${Math.ceil(performance.now() - begin)}ms`)
 }
 
 
